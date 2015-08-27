1272ba44dc0c7ab5ca5c790521ddad6baf1324c9
lewis <ljones140@gmail.com>
2015-08-27 21:45:48 +0100

    basicform input and view of mongo db working

---
+++ myapp/routes/index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lewis is MEAN' , someText: 'hello everyone'});
+});
+
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', {title: 'Hello, World!'});
+});
+
+/* GET Userlist page */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
+/*GET New User page */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', {title: 'Add new User'});
+});
+
+/*POST to add user */
+router.post('/adduser', function(req, res) {
+
+  //set internal db variable
+  var db = req.db;
+
+  //get values from the form
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  //set our collection
+  var collection = db.get('usercollection');
+
+  //insert into database
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function(err, doc) {
+    if (err) {
+      res.send("There was a problem inserting to the database");
+    }
+    else {
+      res.redirect("userlist");
+    }
+  });
+
+});
+
+module.exports = router;
